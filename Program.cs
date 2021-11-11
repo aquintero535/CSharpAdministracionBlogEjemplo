c44b6085ba846449994dccbf76a217def95ab03a
Adrian Quintero <adrianquintero61@gmail.com>
2021-11-11 15:06:37 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace EFGetStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                Console.WriteLine($"Database path: {db.DbPath}.");
+                Console.WriteLine("Inserting a new blog");
+                db.Add(new Blog { Url = "http://blogs.msdn.com/adonet"});
+                db.SaveChanges();
+
+                Console.WriteLine("Querying for a blog");
+                var blog = db.Blogs
+                    .OrderBy(b => b.BlogId)
+                    .First();
+                
+                Console.WriteLine("Updating the blog and adding a post");
+                blog.Url = "https://devblogs.microsoft.com/dotnet";
+                blog.Posts.Add(new Post { Title = "Hello world!", Content = "I wrote an app using EF Core!"});
+                db.SaveChanges();
+
+                Console.WriteLine("Delete the blog");
+                db.Remove(blog);
+                db.SaveChanges();
+            }
+        }
+    }
+}
